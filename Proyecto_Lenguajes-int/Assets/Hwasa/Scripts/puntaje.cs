d935275b313f2b493047bddbcaeeb8b69c5f027c
JuanEstrada01 <79321459+JuanEstrada01@users.noreply.github.com>
2022-06-04 12:41:45 -0500

    puntaje y follow de camara

---
+++ Proyecto_Lenguajes-int/Assets/Hwasa/Scripts/puntaje.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class puntaje : MonoBehaviour
+{
+ private float puntos;
+ private TextMeshProUGUI texMesh;
+private void Start()
+{
+    texMesh = GetComponent<TextMeshProUGUI>();
+}
+private void Update() {
+  
+    texMesh.text = puntos.ToString("0");
+}
+public void SumarPuntos(float puntosEntrada)
+{
+    puntos += puntosEntrada;
+}
+
+}
--- Proyecto_Lenguajes-int/Assets/Riku/Scripts/Coin.cs
+++ Proyecto_Lenguajes-int/Assets/Riku/Scripts/Coin.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public int scoreGive = 100;
+//[SerializeField] private GameObject efecto;
+[SerializeField] private float cantidadPuntos;
+[SerializeField] private puntaje puntaje;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            /*
+              Destroy(gameObject);
             GameManager.obj.addScore(scoreGive);     
             AudioManager.obj.playCoin();
             //UIManager.obj.updateScore();
             FXManager.obj.showPop(transform.position);
             gameObject.SetActive(false);
+            */
+           puntaje.SumarPuntos(cantidadPuntos);
+         //  Instantiate(efecto, transform.position, Quaternion.identity);
+           Destroy (gameObject);
+            
         }
        
     }
