f5320436db84eec78c0d75ad7f823dd52d11dc13
Riku935 <exodo302.richy@gmail.com>
2022-06-08 23:00:45 -0500

    Subiendo el login y una tabla sql

--- Astronauta.sql
+++ Astronauta.sql
@@ -8,7 +8,21 @@ CREATE TABLE Player(
     player_name VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+
 INSERT INTO Player (player_id, player_score, player_name) VALUES
 (1, '400','Luis');
 
 SELECT * FROM Player;
+
+
+
+CREATE TABLE users (
+  id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  username varchar(50) NOT NULL,
+  passwordd varchar(200) NOT NULL,
+);
+
+
+INSERT INTO users (username, passwordd) VALUES
+( 'Luis', '1234' ),
+( 'Ricardo', '5678' );
